18014d58ad0213e94a6612d4272b405d912f99f2
Harsh Devisha <harshdevisha91@gmail.com>
2024-04-06 23:22:25 -0700

    prettier

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,18 @@
-import { validateNumberOfArguments } from '../src/validations';
+import { validateNumberOfArguments } from "../src/validations";
 import { strict as assert } from "assert";
 import ArgumentError from "../src/ArgumentError";
 
-describe('validations', () => {
-  it('should check number of command line arguments', () => {
-    try {
-      validateNumberOfArguments(['1', '2', '3']);
-    } catch (e: any) {
-      assert.strictEqual((e as ArgumentError).message, "must supply 4 arguments");
-    }
+describe("validations", () => {
+  describe("command line arguments", function () {
+    it("should check number of command line arguments", () => {
+      try {
+        validateNumberOfArguments(["1", "2", "3"]);
+      } catch (e: any) {
+        assert.strictEqual(
+          (e as ArgumentError).message,
+          "must supply 4 arguments"
+        );
+      }
+    });
   });
 });
